c38312c5bd23dc796b5a16b37e6b1df3b94430f0
kwakuAddo23 <kaddodanquah@gmail.com>
2023-02-16 19:53:14 -0800

    Task 4 - first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - to print a line of code using the puts function
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
